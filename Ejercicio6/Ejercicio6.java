a8ce86a52f1b53772ede6e1b764e3f0c6a2bcf13
LudvinBaquiax56 <vasqueznicolas56@gmail.com>
2021-06-09 00:16:12 -0600

    Ejercicio6 codificado, compilado y ejecutado

---
+++ Ejercicio6/Ejercicio6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+
+public class Ejercicio6 {
+
+	public static void main(String args[]) {
+		double numero1;
+		double numeroaumentado;
+		numero1 = Math.random() * 200;
+		System.out.println("El numero aleatorio es " + numero1);
+		numeroaumentado = 1.3 * numero1;
+		System.out.println("El numero " + numero1 + " aumentado en un 30% es " + numeroaumentado);
+	}
+
+}
+
